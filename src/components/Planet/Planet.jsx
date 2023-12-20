88522f515279741146d233a0ce65fa5ffc254ff3
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2023-12-20 20:52:41 +0530

    Created mini NavBar in destination page

--- src/components/Planet/Planet.jsx
+++ src/components/Planet/Planet.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import HeadingTwo from '../HeadingTwo/HeadingTwo'
 import BodyText from '../BodyText/BodyText'
+import { Link } from 'react-router-dom'
+import './Planet.css';
 
 const Planet = ({ image }) => {
   return (
-    <div style={{ display: 'flex', marginTop: 90, justifyContent: 'space-between', marginLeft: 300, marginRight: 250 }}>
+    <div style={{ display: 'flex', marginTop: 90, justifyContent: 'space-between', marginLeft: 300, marginRight: 250, alignItems: 'center' }}>
       <img style={{ width: 400, height: 400 }} src={image} alt="" />
 
-      <div style={{}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+
+        <div>
+          <nav className='planet-navbar'>
+            <span className='planet-navbar-item' to={"/destination"} >Moon</span>
+            <span className='planet-navbar-item' to={"/destination"} >Mars</span>
+            <span className='planet-navbar-item' to={"/destination"} >Europa</span>
+            <span className='planet-navbar-item' to={"/destination"} >Titan</span>
+
+          </nav>
+
+
+        </div>
         <HeadingTwo text={"Moon"} />
         <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
 
         <div style={{ width: 400, height: 1, backgroundColor: '#383B4B', marginTop: 50 }}></div>
 
-        <div style={{display: 'flex', width: 400, marginTop: 20}}>
-          <div style={{width: 200}}>
+        <div style={{ display: 'flex', width: 400, marginTop: 20 }}>
+          <div style={{ width: 200 }}>
             <p style={{ fontSize: 14, textTransform: 'uppercase', letterSpacing: 2.36 }}>Avg. Distance</p>
             <h5 style={{ color: 'white', fontSize: 28, fontFamily: 'Bellefair', fontWeight: 'lighter', textTransform: 'uppercase' }}>384,400 km</h5>
           </div>
 
-          <div style={{width: 200}}>
+          <div style={{ width: 200 }}>
             <p style={{ fontSize: 14, textTransform: 'uppercase', letterSpacing: 2.36 }}>Est. travel time</p>
             <h5 style={{ color: 'white', fontSize: 28, fontFamily: 'Bellefair', fontWeight: 'lighter', textTransform: 'uppercase' }}>3 days</h5>
           </div>
--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -3,13 +3,25 @@ import './Destination.css'
 import Moon from './../../assets/image-moon.png';
 import Title from '../../components/Title/Title';
 import Planet from '../../components/Planet/Planet';
+import data from '../../data/data.json';
+import { Link } from 'react-router-dom';
 
 function Destination() {
+
+
   return (
     <section className='destinationpage'>
 
 
     <Title page={"01"} title={"Pick your destination"}/>
+
+    {/* <nav className='planet-navbar'>
+       <Link className='planet-navbar-item' to={"/destination"} >Moon</Link>
+       <Link to={"/destination"} >Mars</Link>
+       <Link to={"/destination"} >Europa</Link>
+       <Link to={"/destination"} >Titan</Link>
+
+    </nav> */}
 
     <Planet image={Moon} />
 
